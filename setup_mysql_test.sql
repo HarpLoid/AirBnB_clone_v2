4adbc494216804ae014710c22ebeebd6e64ea688
HarpLoid <walesii19@gmail.com>
2023-09-19 21:14:31 +0100

    setup for mysql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,15 @@
+-- Script prepares MySQL server
+-- DATABASE hbnb_test_db
+-- User hbnb_test (in localhost)
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER
+IF NOT EXISTS 'hbnb_test'@'localhost'
+	IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.*
+TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_test'@'localhost';

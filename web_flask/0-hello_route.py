c91481720f176ce6b99c150c6623834834a95b2d
HarpLoid <walesii19@gmail.com>
2023-10-20 23:04:09 +0100

    style fixed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,12 +8,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """
     Dislays Hello HBNB!
     """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)

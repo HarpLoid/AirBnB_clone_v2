a24cf58d2d11b00016a70025a7b7360dcf1d512d
HarpLoid <walesii19@gmail.com>
2023-10-21 23:45:28 +0100

    fixed style errer

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -108,8 +108,12 @@ def cities_by_states():
     """
     Displays a HTML page: (inside the tag BODY)
         H1 tag: “States”
-        UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
-        LI tag: description of one State: <state.id>: <B><state.name></B> + UL tag: with the list of City objects linked to the State sorted by name (A->Z)
+        UL tag: with the list of all State objects
+        present in DBStorage sorted by name (A->Z) tip
+        LI tag: description of one
+        State: <state.id>: <B><state.name></B> +
+        UL tag: with the list of City objects
+        linked to the State sorted by name (A->Z)
         LI tag: description of one City: <city.id>: <B><city.name></B>
     """
     from models import storage

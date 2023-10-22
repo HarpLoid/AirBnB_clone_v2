bd38ed78ed114d8f4f78da2b0cff546be882c9ff
HarpLoid <walesii19@gmail.com>
2023-10-22 14:34:33 +0100

    fixed pycodestyle

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -143,5 +143,6 @@ def statesID(id):
     return render_template('9-states.html',
                            state_obj=state_obj)
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
